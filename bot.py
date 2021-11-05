e4f06c7e526634744645c65c3c1d5b14835685d3
Eykuys <93785215+Eykuys@users.noreply.github.com>
2021-11-05 17:45:37 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import tweepy
+CONSUMER_KEY = 'zkbYgY2KH2sSZDzlJeAPtxoTU'
+CONSUMER_SECRET = 'rUUx3VJZ32evg6Tw079tY573OfzmWr7qlYVO1g7fFz3O6cGGfF'
+ACCESS_KEY = '1456598880754278401-hmCWvGAnJ40QcN64ny4KcvmazgmrPj'
+ACCESS_SECRET = 'PSj6ce22Ws9D6iT7Aj2uorVIGLmW1NiT9DJbceJ6SuUxj'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+url = 'https://countrymeters.info/en/World'
+while True:
+ reponse = requests.get(url)
+ soup = BeautifulSoup(reponse.text,"html.parser")
+ item=soup.find('td')
+ population=item.div.text
+ time.sleep(15)
+ tweet = 'The current world population is %s' %(population)
+ api.update_status(tweet)#
